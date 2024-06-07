988797fd4e2f64c298497281527a85fbfbccf649
arnel <thesantos395@gmail.com>
2024-06-07 16:41:41 +0800

    finalized layout, need to debug adding function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
-const myLibrary = [];
+const myBooks = [];  // for book objects storage
 
+
+
+//Declared functions----
 function Book(title,author,pages,read) {
   // the constructor...
   this.title = title;
@@ -8,20 +11,106 @@ function Book(title,author,pages,read) {
   this.read = read;
 }
 
-function addBookToLibrary(title,author,pages,read) {
+function addBookTomyBooks(title,author,pages,read) {
   // do add books to library 
   const addBook = new Book(title,author,pages,read);
-  myLibrary.push(addBook);
+  myBooks.push(addBook);
 }
 
 //pre-load sample books
-addBookToLibrary("moby dick","stan lee","123",true);
-addBookToLibrary("atomic habits","james clear","123",false);
-addBookToLibrary("3 little pigs","beo wolf","435",false);
-
- = element.querySelector(selector)
-
-
+function loadSampleBooks() {
+addBookTomyBooks("moby dick","stan lee","123",true);
+addBookTomyBooks("atomic habits","james clear","123",false);
+addBookTomyBooks("3 little pigs","beo wolf","435",false);
+}
 
 
-console.log(myLibrary);
+function displayBook(key, value){
+//Display myBook stored items
+const booksGrid = document.querySelector(".books-grid");
+//Create bookCard Elements
+const bookCard = document.createElement('div')
+const cardTitle = document.createElement('p')
+const cardAuthor= document.createElement('p')
+const cardPages= document.createElement('p')
+const cardBtnDiv = document.createElement('div')
+const cardRead = document.createElement('button')
+const cardDelete = document.createElement('button')
+//append respective bookCard Childrens
+bookCard.appendChild(cardTitle);
+bookCard.appendChild(cardAuthor);
+bookCard.appendChild(cardPages);
+bookCard.appendChild(cardBtnDiv);
+cardBtnDiv.appendChild(cardRead);
+cardBtnDiv.appendChild(cardDelete);
+//input myBook info txt
+cardTitle.innerText = key.title;
+cardAuthor.innerText = key.author;
+cardPages.innerText = key.pages;
+cardRead.innerText = "Read";
+cardDelete.innerText = "Delete"
+
+
+//add bookCard classlists
+bookCard.classList.add('book-card');
+cardBtnDiv.classList.add('card-btns');
+cardRead.classList.add('btn');
+cardDelete.classList.add('btn');
+cardRead.classList.add('btn-read');
+cardDelete.classList.add('btn-del');
+cardDelete.setAttribute("id", "delBtn");
+cardRead.setAttribute("id", "readBtn");
+
+//Add to existing Html DOM
+booksGrid.appendChild(bookCard);
+}
+
+
+// Close Modal When Clicked on Modal:Background / Cancel Button
+const cancelButton = document.querySelector("#cancelBtn")
+const dialog = document.querySelector("dialog")
+dialog.addEventListener("click", e => {
+  const dialogDimensions = dialog.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    dialog.close()
+  }
+})
+cancelButton.addEventListener('click', () => {
+  dialog.close();
+})
+
+//Open Add Form on Modal 
+const addButton = document.querySelector("#addBookBtn")
+  addButton.addEventListener('click', () => {
+  dialog.showModal();
+  })
+
+//add Book to myBooks Array  
+const submitButton = document.querySelector("#submitBtn")
+submitButton.addEventListener('click', () => {
+     newTitle = document.querySelector("#title").value;
+     newAuthor = document.querySelector("#author").value;
+     newPages = document.querySelector("#pages").value;
+  
+    addBookTomyBooks(newTitle,newAuthor,newPages,false);
+    addBookTomyBooks("7 habits","james clear","123",false);
+    myBooks.forEach(displayBook);
+    
+})
+
+
+
+
+
+
+
+loadSampleBooks();
+
+myBooks.forEach(displayBook);
+console.log(myBooks);
+
